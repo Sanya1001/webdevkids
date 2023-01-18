9b013515e0328a480f2f239b2d6308f95efb182e
Sania Sinha <saniasinha2003@gmail.com>
2023-01-18 19:54:03 +0530

    Updating Links

--- links-script.js
+++ links-script.js
@@ -1,1 +1,16 @@
 // https://codepen.io/alvarotrigo/pen/bGrXmwM
+
+const acc = document.getElementsByClassName('sol');
+
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
